50a5445e84ec341245c126affbfabd9c56663f88
vitorBud <vitor.probem@gmail.com>
2026-02-05 22:09:04 -0300

    aprendendo bastante react e twind

---
+++ SiteIphoneReact/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+
+
+function Hero() {
+    return (
+        <section>
+            <div>
+                <img src="../../img/hero.jpg" alt="" />
+            </div>
+        </section>
+    )
+}
+
+export default Hero
---
+++ SiteIphoneReact/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+function NavBar() {
+    return (
+        <nav className=" fixed top-0 w-full bg-black/80 backdrop-blur-md z-50">
+
+            <div className=" max-w-7xl mx-auto px-6 py-4 flex items-center justify-center gap-8">
+                <a href="#design" className=" hover:text-gray-300">Design</a>
+                <a href="#camera" className=" hover:text-gray-300">Camera</a>
+                <a href="#cores" className=" hover:text-gray-300">Cores</a>
+                <a href="#performance" className=" hover:text-gray-300">Performance</a>
+                <button className=" bg-blue-600 hover:bg-blue-700 px-6 py-2 rounded-full">Comprar</button>
+            </div>
+
+        </nav >
+
+    )
+}
+
+export default NavBar
