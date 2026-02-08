916d791535fe7a64e00fb5d513f6fd6599acb20d
vitor Gomes <vitor.probem@gmail.com>
2026-02-08 18:23:22 -0300

    arrumando margin do background

--- SiteIphoneReact/src/components/Hero.jsx
+++ SiteIphoneReact/src/components/Hero.jsx
@@ -2,9 +2,16 @@
 
 function Hero() {
     return (
-        <section>
-            <div>
-                <img src="../../img/hero.jpg" alt="" />
+        <section className=" relative h-screen overflow-hidden bg-black">
+            <div className=" top-18 bottom-0 right-0 left-0 z-0 mt-25">
+                <img src="../../img/hero.jpg" className=" w-full h-full object-cover opacity-90" />
+                <div className="absolute inset-0 bg-gradient-to-b from-black/30 via-transparent to-black/40"></div>
+            </div>
+
+            <div className=" absolute bottom-8 left-1/2 z-10 animate-bounce">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+                </svg>
             </div>
         </section>
     )
--- SiteIphoneReact/src/components/NavBar.jsx
+++ SiteIphoneReact/src/components/NavBar.jsx
@@ -1,6 +1,6 @@
 function NavBar() {
     return (
-        <nav className=" fixed top-0 w-full bg-black/80 backdrop-blur-md z-50">
+        <nav className="fixed top-0 w-full bg-black/80 backdrop-blur-md z-50">
 
             <div className=" max-w-7xl mx-auto px-6 py-4 flex items-center justify-center gap-8">
                 <a href="#design" className=" hover:text-gray-300">Design</a>
