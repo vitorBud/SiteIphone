15aa44d6432566e1a101c9b38a86ba567e1a0ac2
Vitor Gomes de Alcântara <vitor.probem@gmail.com>
2026-02-11 17:10:00 -0300

    performance

--- SiteIphoneReact/src/components/Highlights.jsx
+++ SiteIphoneReact/src/components/Highlights.jsx
@@ -11,18 +11,32 @@ function Highlights() {
 
                 <div className="grid grid-cols-2 gap-8 mb-16">
                     <div className=" bg-gray-900 rounded-3xl p-8">
-                        <img className=" w-full rounded-2xl mb-4" src="../../public/img/titanium-design.jpg" alt="iphone titanium"></img>
+                        <img className=" w-full rounded-2xl mb-4" src="../../img/titanium-design.jpg" alt="iphone titanium"></img>
                         <h3 className=" font-bold mb-2 text-3xl">Titanium Premium</h3>
                         <p className="text-gray-300">Estrutura em titanio de grau aeroespacial. O smartphone mais forte e leve</p>
                     </div>
 
                     <div className=" bg-gray-900 rounded-3xl p-8">
-                        <img src="../../public/img/ios-features.jpg" alt="ios 2025" />
+                        <img className="w-full rounded-2xl mb-4" src="../../img/ios-features.jpg" alt="ios 2025" />
                         <h3 className=" font-bold mb-2 text-3xl">IOS 26</h3>
                         <p className="text-gray-300">Sistema operacional mais avançado do mundo com IA integrada. </p>
                     </div>
                 </div>
 
+            </div>
+
+
+            <div className=" bg-gray-900 rounded-3xl p-12 mb-16" id="performance">
+                <h3>A18 Pro</h3>
+                <p>O chip mais poderoso em um smartphone</p>
+                <img className=" rounded-2xl mb-4" src="../../img/chip-a18-pro.jpg" alt="chip a18" />
+
+                <ul>
+                    <li>CPU 20% mais rápida</li>
+                    <li>GPU 25% mais eficiente</li>
+                    <li>Neural Engine com 16 núcleos</li>
+                    <li>Ray TRacing acelerado por hardware</li>
+                </ul>
             </div>
         </section>
     )
